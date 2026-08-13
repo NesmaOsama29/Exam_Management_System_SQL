41b266852f775684cc04d1ee5fbd4eb9efdfd2ed
Nesma Osama <Nesma Osama@NESMA>
2026-08-13 15:57:44 +0300

    Add project files.

---
+++ ReportsForm/MainForm.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace ReportsForm
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 650);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exam Management System - Dashboard";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ ReportsForm/StudentExamForm.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ReportsForm
+{
+    public partial class StudentExamForm : Form
+    {
+        private ReportViewer reportViewerResults;
+        private string connString = @"Server=localhost;Database=ExamManagementDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public StudentExamForm()
+        {
+            InitializeComponent();
+
+            reportViewerResults = new ReportViewer();
+            reportViewerResults.Dock = DockStyle.Fill;
+            this.Controls.Add(reportViewerResults);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            LoadStudentResultsReport();
+        }
+
+        private void StudentExamForm_Load(object sender, EventArgs e)
+        {
+            LoadStudentResultsReport();
+        }
+
+        private void LoadStudentResultsReport()
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            try
+            {
+                DataTable dt = new DataTable();
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    string query = "SELECT * FROM v_StudentExamResults";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    da.Fill(dt);
+                }
+
+                reportViewerResults.LocalReport.DataSources.Clear();
+                reportViewerResults.LocalReport.ReportPath = @"Reports\Student Exam Results Report.rdl";
+                ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+                reportViewerResults.LocalReport.DataSources.Add(rds);
+
+                reportViewerResults.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading student exam results report: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
